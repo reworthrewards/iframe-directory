857bae73039ea1db5b74af53f21c8698d6c7fea8
Rodrigo Salmeron <rodd.sal23@gmail.com>
2021-11-26 17:02:21 -0600

    regex hex color

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import {useLocation} from "react-router-dom";
 function App() {
 
   const search = useLocation().search;
+  const hexRegex = /^[0-9A-F]{6}$/i;
   const accentColor = new URLSearchParams(search).get('accentColor');
+  const accentColorValid = accentColor === null || accentColor === undefined ? undefined : hexRegex.test(accentColor);
   const filters = new URLSearchParams(search).get('filters');
   const arrayFilter = filters === null || filters === '' ? undefined : filters.split(',');
   const lang = new URLSearchParams(search).get('lang');
@@ -14,7 +16,7 @@ function App() {
 
   return (
     <ReworthDirectory
-      accentColor={accentColor === null || accentColor === '' ? undefined : `#${accentColor}`}
+      accentColor={accentColorValid === true ? `#${accentColor}` : undefined}
       lang={lang === null || lang === '' ? undefined : lang === 'ES' ? lang : lang === 'EN' ? lang : undefined} 
       filters={arrayFilter} 
       showHeader={showHeaderBool} 
