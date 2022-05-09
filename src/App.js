1987b8e40dd09648bd379f92c8b7d467b0cc4cda
Rodrigo Salmeron <rodd.sal23@gmail.com>
2022-05-09 16:15:16 -0500

    env flavor working

--- src/App.js
+++ src/App.js
@@ -9,12 +9,28 @@ function App() {
   const accentColorValid = accentColor === null || accentColor === undefined ? undefined : hexRegex.test(accentColor);
   const fontFamily = new URLSearchParams(search).get('fontFamily');
   const lang = new URLSearchParams(search).get('lang');
+  const env = new URLSearchParams(search).get('env');
+
+  const availableEnvs = ['legacy', 'prod', 'dev', 'local'];
+
+  const handleEnv = () => {
+    let baseEnv = '';
+    if(env) {
+      availableEnvs.forEach((item) => {
+        if(item === env) {
+          baseEnv = item
+        }
+      })
+      return baseEnv
+    }
+  }
 
   return (
     <ReworthDirectory
       accentColor={accentColorValid === true ? `#${accentColor}` : undefined}
       fontFamily={fontFamily === null || fontFamily === undefined || fontFamily === "undefined" ? undefined : fontFamily}
       lang={lang === null || lang === '' ? undefined : lang === 'ES' ? lang : lang === 'EN' ? lang : undefined} 
+      env={handleEnv()}
     />
   );
 }
