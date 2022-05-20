a9f910285d1d381048f11b94ad8f2710ba80de6d
Rodrigo Salmeron <rodd.sal23@gmail.com>
2022-05-20 13:39:09 -0500

    new support for falvors and dynamic env

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { ReworthDirectory } from '@reworthrewards/reworth-directory';
 import {useLocation} from "react-router-dom";
 
 function App() {
-
   const search = useLocation().search;
   const hexRegex = /^[0-9A-F]{6}$/i;
   const accentColor = new URLSearchParams(search).get('accentColor');
